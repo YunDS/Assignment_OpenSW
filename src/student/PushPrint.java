39a05042c93083ea8f57b8cee383d85b46dd9759
yds92 <yds92@211.225.89.40>
2019-11-20 07:26:09 +0900

    added new Class File on master branch in Local

--- src/student/PushPrint.java
+++ src/student/PushPrint.java
@@ -3,7 +3,7 @@ package student;
 public class PushPrint {
 
 	public static void main(String[] args) {
-		System.out.println("Add New Java Class File in Local -> Online");
+		System.out.println("Add New Java Class File in Local to Online");
 
 	}
 
