527b3e43fada724650b20df8b236a0ccff0e920f
yds92 <yds92@211.225.89.40>
2019-11-20 07:31:13 +0900

    PushPrint.java modified on Local repository

--- src/student/PushPrint.java
+++ src/student/PushPrint.java
@@ -4,6 +4,7 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("Add New Java Class File in Local to Online");
+		System.out.println("This line has been written in Local World");
 
 	}
 
